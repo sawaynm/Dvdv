ccdf8bc7746fcb0b6e09a38505bcddaafe8f6580
Sourabh Panchal <sourabh.panchal28@gmail.com>
2024-11-10 01:27:02 +0530

    clean: removing useless code

--- src/com/offsec/nethunter/AudioFragment.java
+++ src/com/offsec/nethunter/AudioFragment.java
@@ -212,17 +212,6 @@ public class AudioFragment extends Fragment {
         return formattedValues;
     }
 
-    private String getDayWithSuffix(int day) {
-        if (day >= 11 && day <= 13) {
-            return day + "th";
-        }
-        switch (day % 10) {
-            case 1: return day + "st";
-            case 2: return day + "nd";
-            case 3: return day + "rd";
-            default: return day + "th";
-        }
-    }
 
     private void updatePrefs(AudioPlaybackService service) {
         String serverPref = service.getServerPref();
