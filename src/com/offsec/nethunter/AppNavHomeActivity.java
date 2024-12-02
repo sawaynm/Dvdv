728a6f6fc456e0b1957240d7b640d63376188579
yesimxev <yesimxev@github.com>
2024-12-02 23:27:20 +0000

    Fix menu crash due to mitmf removal

--- src/com/offsec/nethunter/AppNavHomeActivity.java
+++ src/com/offsec/nethunter/AppNavHomeActivity.java
@@ -387,13 +387,13 @@ public class AppNavHomeActivity extends AppCompatActivity implements KaliGPSUpda
             if (model.equals("catfish") || model.equals("catshark") || model.equals("catshark-4g")) navigationView.getMenu().getItem(8).setVisible(false);
             navigationView.getMenu().getItem(9).setVisible(false);
             navigationView.getMenu().getItem(14).setVisible(false);
+            navigationView.getMenu().getItem(16).setVisible(false);
             navigationView.getMenu().getItem(17).setVisible(false);
-            navigationView.getMenu().getItem(18).setVisible(false);
+            navigationView.getMenu().getItem(19).setVisible(false);
             navigationView.getMenu().getItem(20).setVisible(false);
             navigationView.getMenu().getItem(21).setVisible(false);
             navigationView.getMenu().getItem(22).setVisible(false);
             navigationView.getMenu().getItem(23).setVisible(false);
-            navigationView.getMenu().getItem(24).setVisible(false);
         } else {
             snowfall = prefs.getBoolean("snowfall_enabled", true);
         }
